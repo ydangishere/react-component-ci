a2c9c2071ae7fbb74fd5cbaa59099fd782e99cb7
y van dang <160577556+ydangishere@users.noreply.github.com>
2025-04-27 12:46:16 +0700

    Update HelloWorld.test.jsx

--- src/components/HelloWorld.test.jsx
+++ src/components/HelloWorld.test.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
 import HelloWorld from './HelloWorld';
 
 test('renders hello world text', () => {
